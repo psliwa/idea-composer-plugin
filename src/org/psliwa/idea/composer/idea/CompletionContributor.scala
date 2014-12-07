f3f79c39aa92fe12e53aa06a60c4f4300367390e
psliwa <peter.pl7@gmail.com>
2014-12-07 21:35:24 +0100

    completion refactoring, support for arrays, nested objects etc

--- src/org/psliwa/idea/composer/idea/CompletionContributor.scala
+++ src/org/psliwa/idea/composer/idea/CompletionContributor.scala
@@ -3,36 +3,56 @@ package org.psliwa.idea.composer.idea
 import com.intellij.codeInsight.completion._
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.json.JsonLanguage
-import com.intellij.json.psi.JsonElement
+import com.intellij.json.psi._
 import com.intellij.lang.{ASTNode, Language}
 import com.intellij.openapi.util.{TextRange, Key}
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns._
+import com.intellij.patterns.{PsiElementPattern, PlatformPatterns}
 import com.intellij.psi.impl.source.tree.{LeafPsiElement, PsiWhiteSpaceImpl}
 import com.intellij.psi._
 import com.intellij.psi.search.{SearchScope, GlobalSearchScope}
 import com.intellij.util.ProcessingContext
-import org.psliwa.idea.composer.parser.{SStringChoice, SObject, Schema, SchemaLoader}
+import org.psliwa.idea.composer.parser._
 
 import scala.annotation.tailrec
 
 class CompletionContributor extends com.intellij.codeInsight.completion.CompletionContributor {
   private lazy val schema = SchemaLoader.load()
+  private val emptyNamePlaceholder = "IntellijIdeaRulezzz"
 
-  extend(
-    CompletionType.BASIC,
-    PlatformPatterns.psiElement(classOf[PsiElement]).withLanguage(JsonLanguage.INSTANCE),
-    new CompletionProvider[CompletionParameters]() {
-      override def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet): Unit = {
-        if(parameters.getOriginalFile.getName == "composer.json") {
-          getCompletionsFor(parameters.getOriginalPosition)
-            .foreach(s => result.addElement(
-              //TODO: support for quotes
-              LookupElementBuilder.create(s)
-            ))
-        }
-      }
+  type Capture = PsiElementPattern.Capture[_ <: PsiElement]
+
+  private def loop(s: Schema, parent: Capture): List[(Capture, CompletionProvider[CompletionParameters])] = s match {
+    case SObject(m) => {
+      val propertyCapture = psiElement(classOf[JsonProperty]).withParent(psiElement(classOf[JsonObject]).withParent(parent))
+
+      List(
+        (psiElement().withSuperParent(2,
+          psiElement().and(propertyCapture).andOr(
+            psiElement().withName(emptyNamePlaceholder),
+            psiElement().withName(emptyNamePlaceholder+" ")
+          )),
+          KeywordsCompletionProvider(m.keys))
+      ) ++  m.flatMap(t => loop(t._2, psiElement().and(propertyCapture).withName(t._1)))
     }
-  )
+    case SStringChoice(m) => List((psiElement().withSuperParent(2, parent), KeywordsCompletionProvider(m)))
+    case SOr(l) => l.flatMap(loop(_, parent))
+    case SArray(i) => loop(i, psiElement(classOf[JsonArray]).withParent(parent))
+    case SBoolean => List((psiElement().withSuperParent(2, parent).afterLeaf(":"), KeywordsCompletionProvider(List("true", "false"))))
+    case _ => List()
+  }
 
-  private def getCompletionsFor(e: PsiElement): List[String] = schema.map(Completion.getCompletionsFor(_)(e)).getOrElse(List())
+  schema.foreach(loop(_,
+    psiElement(classOf[JsonFile])
+      .withLanguage(JsonLanguage.INSTANCE)
+      .inFile(psiFile(classOf[JsonFile]).withName("composer.json"))
+  ).foreach {
+    case (pattern, provider) => extend(CompletionType.BASIC, pattern, provider)
+  })
+
+  private case class KeywordsCompletionProvider(keywords: Iterable[String]) extends CompletionProvider[CompletionParameters] {
+    override def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet): Unit = {
+      keywords.foreach(k => result.addElement(LookupElementBuilder.create(k).bold))
+    }
+  }
 }
--- tests/org/psliwa/idea/composer/test/idea/CompletionContributorTest.scala
+++ tests/org/psliwa/idea/composer/test/idea/CompletionContributorTest.scala
@@ -15,11 +15,12 @@ class CompletionContributorTest extends LightPlatformCodeInsightFixtureTestCase 
     )
   }
 
-  private def completion(contents: String, expectedSuggestions: Array[String]) = {
+  private def completion(contents: String, expectedSuggestions: Array[String], unexpectedSuggestions: Array[String] = Array()) = {
     myFixture.configureByText("composer.json", contents)
     myFixture.completeBasic()
 
     assertContainsElements(myFixture.getLookupElementStrings, expectedSuggestions:_*)
+    assertDoesntContain(myFixture.getLookupElementStrings, unexpectedSuggestions:_*)
   }
 
   def testCompletionOnTopLevelWithQuotes() = {
@@ -40,7 +41,8 @@ class CompletionContributorTest extends LightPlatformCodeInsightFixtureTestCase 
         |   "type": <caret>
         | }
       """.stripMargin,
-      Array("library", "project")
+      Array("library", "project"),
+      Array("name", "type")
     )
   }
 
@@ -51,7 +53,8 @@ class CompletionContributorTest extends LightPlatformCodeInsightFixtureTestCase 
         |   "type": <caret>,
         | }
       """.stripMargin,
-      Array("library", "project")
+      Array("library", "project"),
+      Array("name", "type")
     )
   }
 
@@ -62,7 +65,8 @@ class CompletionContributorTest extends LightPlatformCodeInsightFixtureTestCase 
         |   "type": "<caret>"
         | }
       """.stripMargin,
-      Array("library", "project")
+      Array("library", "project"),
+      Array("name", "type")
     )
   }
 
@@ -73,7 +77,72 @@ class CompletionContributorTest extends LightPlatformCodeInsightFixtureTestCase 
         |   "type": "<caret>",
         | }
       """.stripMargin,
-      Array("library", "project")
+      Array("library", "project"),
+      Array("name", "type")
+    )
+  }
+
+  def testNestedObjectCompletion() = {
+    completion(
+      """
+        |{
+        | "support": {
+        |   <caret>
+        | }
+        |}
+      """.stripMargin,
+      Array("email", "issues"),
+      Array("type")
+    )
+  }
+
+  def testOrCompletion_firstAlternative() = {
+    completion(
+      """
+        |{
+        | "license": <caret>
+        |}
+      """.stripMargin,
+      Array("MIT", "BSD")
+    )
+  }
+
+  def testOrCompletion_secondAlternative() = {
+    completion(
+      """
+        |{
+        | "license": [
+        |   <caret>
+        | ]
+        |}
+      """.stripMargin,
+      Array("MIT", "BSD")
+    )
+  }
+
+  def testBooleanCompletion() = {
+    completion(
+      """
+        |{
+        | "prefer-stable": <caret>
+        |}
+      """.stripMargin,
+      Array("true", "false")
+    )
+  }
+
+  def testArrayCompletion() = {
+    completion(
+      """
+        |{
+        | "authors": [
+        |   {
+        |     <caret>
+        |   }
+        | ]
+        |}
+      """.stripMargin,
+      Array("name", "email", "homepage")
     )
   }
 }
