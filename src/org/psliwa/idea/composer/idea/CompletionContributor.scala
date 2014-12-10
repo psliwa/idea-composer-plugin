a95d942b81c21ee56bbc490af2eac294d796f583
psliwa <peter.pl7@gmail.com>
2014-12-10 19:38:35 +0100

    refactoring

--- src/org/psliwa/idea/composer/idea/CompletionContributor.scala
+++ src/org/psliwa/idea/composer/idea/CompletionContributor.scala
@@ -6,79 +6,87 @@ import com.intellij.json.JsonLanguage
 import com.intellij.json.psi._
 import com.intellij.patterns.PlatformPatterns._
 import com.intellij.patterns.StandardPatterns._
-import com.intellij.patterns.{PatternCondition, PsiElementPattern}
+import com.intellij.patterns.{PsiElementPattern, PatternCondition}
 import com.intellij.psi._
 import com.intellij.util.ProcessingContext
 import org.psliwa.idea.composer.packagist.Packagist
 import org.psliwa.idea.composer.schema._
 
 class CompletionContributor extends com.intellij.codeInsight.completion.CompletionContributor {
-  private lazy val schema = SchemaLoader.load()
+  import CompletionContributor._
+
   private val emptyNamePlaceholder = "IntellijIdeaRulezzz"
 
+  private lazy val schema = SchemaLoader.load()
+  private lazy val packages = loadPackages().map(Keyword(_))
   private var loadPackages: () => List[String] = () => Packagist.load().right.toOption.getOrElse(List())
 
-  private lazy val packages = loadPackages().map(Keyword(_))
+  schema.foreach(addCompletionProvidersForSchema)
 
-  type Capture = PsiElementPattern.Capture[_ <: PsiElement]
-
-  private def loop(s: Schema, parent: Capture): List[(Capture, CompletionProvider[CompletionParameters])] = s match {
-    case SObject(m) => {
-      val propertyCapture = psiElement(classOf[JsonProperty]).withParent(psiElement(classOf[JsonObject]).withParent(parent))
-
-      List(
-        (
-          psiElement().withSuperParent(2, psiElement().and(propertyCapture)
-            .andOr(
-              psiElement().withName(string().`with`(new PatternCondition[String]("contains") {
-                override def accepts(t: String, context: ProcessingContext): Boolean = t.contains(emptyNamePlaceholder)
-              })),
-              psiElement().withName(emptyNamePlaceholder)
-            )
-          )
-          ,
-          KeywordsCompletionProvider(m.keys.map(Keyword(_)))
-        )
-      ) ++  m.flatMap(t => loop(t._2, psiElement().and(propertyCapture).withName(t._1)))
+  private def addCompletionProvidersForSchema(schema: Schema): Unit = {
+    completionProvidersForSchema(schema, rootPsiElementPattern).foreach {
+      case (pattern, provider) => extend(CompletionType.BASIC, pattern, provider)
     }
-    case SStringChoice(m) => List((psiElement().withSuperParent(2, parent), KeywordsCompletionProvider(m.map(Keyword(_)))))
-    case SOr(l) => l.flatMap(loop(_, parent))
-    case SArray(i) => loop(i, psiElement(classOf[JsonArray]).withParent(parent))
-    case SBoolean => List((psiElement().withSuperParent(2, parent).afterLeaf(":"), KeywordsCompletionProvider(List("true", "false").map(Keyword(_, quoted = false)))))
+  }
+
+  private def completionProvidersForSchema(s: Schema, parent: Capture): List[(Capture, CompletionProvider[CompletionParameters])] = s match {
+    case SObject(m) => {
+      propertyCompletionProviders(parent, () => m.keys.map(Keyword(_))) ++
+        m.flatMap(t => completionProvidersForSchema(t._2, psiElement().and(propertyCapture(parent)).withName(t._1)))
+    }
+    case SStringChoice(m) => List((psiElement().withSuperParent(2, parent), KeywordsCompletionProvider(() => m.map(Keyword(_)))))
+    case SOr(l) => l.flatMap(completionProvidersForSchema(_, parent))
+    case SArray(i) => completionProvidersForSchema(i, psiElement(classOf[JsonArray]).withParent(parent))
+    case SBoolean => List((psiElement().withSuperParent(2, parent).afterLeaf(":"), KeywordsCompletionProvider(() => List("true", "false").map(Keyword(_, quoted = false)))))
     case SPackages => {
-      //TODO: refactoring with SObject - SPackages should be special case of SObject?
-      val propertyCapture = psiElement(classOf[JsonProperty]).withParent(psiElement(classOf[JsonObject]).withParent(parent))
-      List(
-        (
-          psiElement().withSuperParent(2, psiElement().and(propertyCapture)
-            .andOr(
-              psiElement().withName(string().`with`(new PatternCondition[String]("contains") {
-                override def accepts(t: String, context: ProcessingContext): Boolean = t.contains(emptyNamePlaceholder)
-              })),
-              psiElement().withName(emptyNamePlaceholder)
-            )
-          )
-          ,
-          KeywordsCompletionProvider(packages)
-          )
-      )
+      propertyCompletionProviders(parent, () => packages)
     }
     case _ => List()
   }
 
-  schema.foreach(loop(_,
+  private def rootPsiElementPattern: PsiElementPattern.Capture[JsonFile] = {
     psiElement(classOf[JsonFile])
       .withLanguage(JsonLanguage.INSTANCE)
       .inFile(psiFile(classOf[JsonFile]).withName("composer.json"))
-  ).foreach {
-    case (pattern, provider) => extend(CompletionType.BASIC, pattern, provider)
-  })
+  }
 
-  private case class Keyword(text: String, quoted: Boolean = true)
+  private def propertyCompletionProviders(parent: Capture, keywords: Keywords) = {
+    List(
+      (
+        psiElement()
+          .withSuperParent(2,
+            psiElement().and(propertyCapture(parent))
+              .withName(stringContains(emptyNamePlaceholder))
+          ),
+        KeywordsCompletionProvider(keywords)
+      )
+    )
+  }
 
-  private case class KeywordsCompletionProvider(keywords: Iterable[Keyword]) extends CompletionProvider[CompletionParameters] {
+  private def propertyCapture(parent: Capture): PsiElementPattern.Capture[JsonProperty] = {
+    psiElement(classOf[JsonProperty]).withParent(psiElement(classOf[JsonObject]).withParent(parent))
+  }
+
+  protected[idea] def setPackagesLoader(l: () => List[String]): Unit = {
+    loadPackages = l
+  }
+
+  private def stringContains(s: String) = {
+    string().`with`(new PatternCondition[String]("contains") {
+      override def accepts(t: String, context: ProcessingContext): Boolean = t.contains(s)
+    })
+  }
+}
+
+private object CompletionContributor {
+  type Capture = PsiElementPattern.Capture[_ <: PsiElement]
+  type Keywords = () => Iterable[Keyword]
+
+  case class Keyword(text: String, quoted: Boolean = true)
+
+  case class KeywordsCompletionProvider(keywords: Keywords) extends CompletionProvider[CompletionParameters] {
     override def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet): Unit = {
-      keywords.foreach(k => result.addElement(LookupElementBuilder.create(k.text).bold.withInsertHandler(insertHandler(parameters.getPosition, k))))
+      keywords().foreach(k => result.addElement(LookupElementBuilder.create(k.text).bold.withInsertHandler(insertHandler(parameters.getPosition, k))))
     }
 
     private def insertHandler(element: PsiElement, keyword: Keyword) = {
@@ -87,7 +95,7 @@ class CompletionContributor extends com.intellij.codeInsight.completion.Completi
     }
   }
 
-  private object QuoteInsertHandler extends InsertHandler[LookupElement] {
+  object QuoteInsertHandler extends InsertHandler[LookupElement] {
     override def handleInsert(context: InsertionContext, item: LookupElement): Unit = {
       val document = context.getEditor.getDocument
       val editor = context.getEditor
@@ -97,9 +105,5 @@ class CompletionContributor extends com.intellij.codeInsight.completion.Completi
 
       editor.getCaretModel.moveToOffset(context.getStartOffset + item.getLookupString.length + 2)
     }
-  }
-
-  protected[idea] def setPackagesLoader(l: () => List[String]): Unit = {
-    loadPackages = l
   }
 }
