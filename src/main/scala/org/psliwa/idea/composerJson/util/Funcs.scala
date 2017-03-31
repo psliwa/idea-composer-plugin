ed84e04dfe7f09cac0cf40345ed72d0117240002
piotr.sliwa <me@psliwa.org>
2017-03-31 23:14:19 +0200

    [#39] fix possible NullPointer - problem with concurrent access to cache

--- src/main/scala/org/psliwa/idea/composerJson/util/Funcs.scala
+++ src/main/scala/org/psliwa/idea/composerJson/util/Funcs.scala
@@ -1,17 +1,24 @@
 package org.psliwa.idea.composerJson.util
 
 import java.util
+import java.util.Collections
 import java.util.Map.Entry
 
 object Funcs {
   def memorize[A,B](maxSize: Int)(f: A => B): A => B = {
-    val cache = new util.LinkedHashMap[A,B](){
+    val cache = Collections.synchronizedMap(new util.LinkedHashMap[A,B](){
       override def removeEldestEntry(eldest: Entry[A, B]): Boolean = this.size() > maxSize
-    }
+    })
 
     (a: A) => {
-      if(!cache.containsKey(a)) cache.put(a, f(a))
-      cache.get(a)
+      val cachedValue = cache.get(a)
+      if(cachedValue == null) {
+        val newValue = f(a)
+        cache.put(a, newValue)
+        newValue
+      } else {
+        cachedValue
+      }
     }
   }
 }
