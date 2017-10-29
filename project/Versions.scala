1dc54e14ae40706a1e8b01cb06d8b404b998a1f5
piotr.sliwa <me@psliwa.org>
2017-10-29 18:48:28 +0100

    update scala and dependencies versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
-  val scala = "2.12.2"
-  val scalaz = "7.2.8"
-  val scalaParsers = "1.0.5"
+  val scala = "2.12.4"
+  val scalaz = "7.2.16"
+  val scalaParsers = "1.0.6"
   val idea = "2017.2.5"
 }
